bca4e79c219ede1b70f1738957fd852d3458d600
Haroon <haroon.a.amanullah@gmail.com>
2023-11-07 18:37:32 -0500

    mecanum movement working!!!

--- prototypes/mecanum_movement/mecanum_movement.ino
+++ prototypes/mecanum_movement/mecanum_movement.ino
@@ -9,6 +9,7 @@ unsigned long startTime = 0;
 int function_num = 0;
 
 void setup() {
+  Serial.begin(9600);
 }
 
 void loop() {
@@ -18,20 +19,25 @@ void loop() {
   // delay(1000);
 
   float pi = 3.1415926;
+  float power = 1;
 
   // depending on what the function number is, continuously run that function
   if (function_num == 0) {
-    move(pi/2, 1, 0); // should move forward
+    move(pi/2, power, 0); // should move forward
   } else if (function_num == 1) {
-    move(0, 1, 0); // should move right
+    move(0, power, 0); // should move right
   } else if (function_num == 2) {
-    move(-pi/2, 1, 0); // should move backward
+    move(-pi/2, power, 0); // should move backward
   } else if (function_num == 3) {
-    move(pi, 1, 0); // should move left
+    move(pi, power, 0); // should move left
   } else if (function_num == 4) {
-    move(0, 0, 1); // should rotate right
+    move(0, 0, power); // should rotate right
   } else if (function_num == 5) {
-    move(0, 0, -1); // should rotate left
+    move(0, 0, -power); // should rotate left
+  } else if (function_num == 6) {
+    move(5*pi/4, 1, 1);
+  } else if (function_num > 8) {
+    move(0, 0, 0);
   }
 
   if (millis() - startTime >= 1000) { // when 1 second has passed
@@ -40,6 +46,78 @@ void loop() {
   }
       
 }
+
+void move(float theta, float power, float turn) {
+  // define variables for calculations
+  float pi = 3.1415926;
+  float sinV = sin(theta - pi/4);
+  float cosV = cos(theta - pi/4);
+  float maxval = max(abs(sinV),abs(cosV));
+
+  // calculate motor power levels
+  float leftFront   = power * cosV/maxval + turn;
+  float rightFront  = power * sinV/maxval - turn;
+  float leftRear    = power * sinV/maxval + turn;
+  float rightRear   = power * cosV/maxval - turn;
+
+  // normalize motor values if they exceed max power
+  if ((power + abs(turn)) > 1) {
+    leftFront /= power + turn;
+    rightFront /= power + turn;
+    leftRear /= power + turn;
+    rightRear /= power + turn;
+  }
+
+  // multiply final values by 255 to get max speed
+  leftFront *= 255;
+  rightFront *= 255;
+  leftRear *= 255;
+  rightRear *= 255;
+
+  // run motors at proper speeds
+  if (leftFront > 0) {
+    motor1.run(FORWARD);
+  } else {
+    motor1.run(BACKWARD);
+    leftFront = -leftFront;
+  }
+  motor1.setSpeed(int(leftFront));
+
+  motor2.setSpeed(abs(int(rightFront)));
+  if (rightFront > 0) {
+    motor2.run(FORWARD);
+  } else {
+    motor2.run(BACKWARD);
+  }
+
+  motor3.setSpeed(abs(int(leftRear)));
+  if (leftRear > 0) {
+    motor3.run(FORWARD);
+  } else {
+    motor3.run(BACKWARD);
+  }
+
+  motor4.setSpeed(abs(int(rightRear)));
+  if (rightRear > 0) {
+    motor4.run(FORWARD);
+  } else {
+    motor4.run(BACKWARD);
+  }
+
+  // print motor speeds
+  Serial.print("  Left Front: ");
+  Serial.print(leftFront);
+  Serial.print("  Right Front: ");
+  Serial.print(rightFront);
+  Serial.print("  Left Rear: ");
+  Serial.print(leftRear);
+  Serial.print("  Right Rear: ");
+  Serial.print(rightRear);
+
+}
+
+
+//////// OLD //////////////
 
 //Low-level functions
 void runMotor(float value, int motor) {
@@ -108,9 +186,6 @@ void goBackward(float speed_percent, float seconds) { //input PERCENTAGE of 100%
   delay(milliseconds); //run for desired time
 }
 
-
-
-//OLD
 void movementtest(int theta, int power, int turn) {
   //input: theta, power, and turn
   float pi = 3.1415926;
@@ -154,65 +229,3 @@ void movementtest(int theta, int power, int turn) {
   delay(1000);
 }
 
-// NEW MOVE FUNC
-void move(float theta, float power, float turn) {
-  // define variables for calculations
-  float pi = 3.1415926;
-  float sin = sin(theta - pi/4);
-  float cos = cos(theta - pi/4);
-  float maxval = max(abs(sin),abs(cos));
-
-  // calculate motor power levels
-  float leftFront   = power * cos/maxval + turn;
-  float rightFront  = power * sin/maxval - turn;
-  float leftRear    = power * sin/maxval + turn;
-  float rightRear   = power * cos/maxval - turn;
-
-  // normalize motor values if they exceed max power
-  if ((power + abs(turn)) > 1) {
-    leftFront /= power + turn;
-    rightFront /= power + turn;
-    leftRear /= power + turn;
-    rightRear /= power + turn;
-  }
-
-  // run motors at proper speeds
-  motor1.setSpeed(int(leftFront));
-  if (leftFront > 0) {
-    motor1.run(FORWARD);
-  } else {
-    motor1.run(BACKWARD);
-  }
-
-  motor2.setSpeed(int(rightFront));
-  if (rightFront > 0) {
-    motor2.run(FORWARD);
-  } else {
-    motor2.run(BACKWARD);
-  }
-
-  motor3.setSpeed(int(leftRear));
-  if (leftRear > 0) {
-    motor3.run(FORWARD);
-  } else {
-    motor3.run(BACKWARD);
-  }
-
-  motor4.setSpeed(int(rightRear));
-  if (rightRear > 0) {
-    motor4.run(FORWARD);
-  } else {
-    motor4.run(BACKWARD);
-  }
-
-  // print motor speeds
-  Serial.print("  Left Front: ");
-  Serial.print(leftFront);
-  Serial.print("  Right Front: ");
-  Serial.print(rightFront);
-  Serial.print("  Left Rear: ");
-  Serial.print(leftRear);
-  Serial.print("  Right Rear: ");
-  Serial.print(rightRear);
-
-}
