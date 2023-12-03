fa235705d2317da3792db53c1123a1149c11cc9e
Penguinlokilog <148892607+Penguinlokilog@users.noreply.github.com>
2023-12-03 12:28:46 -0500

    12/3 Morning code

--- the-vedauntless/mission.h
+++ the-vedauntless/mission.h
@@ -29,9 +29,9 @@ void navToSite(float distance) {
   // face the other side
   if (v) {
     if (y <= 1) {
-      turnToTheta(pi/2, pi/20);
+      turnToTheta(pi / 2, pi / 20);
     } else {
-      turnToTheta(3*pi/2, pi/20);
+      turnToTheta(3 * pi / 2, pi / 20);
     }
   }
 
@@ -51,7 +51,7 @@ void turnToFace(float timerange, float error) {
 
   bool parallel = false;
 
-  moveWithTime(pi, 0.5, 0, timerange/2); //center
+  moveWithTime(pi, 0.5, 0, timerange / 2);  //center
 
   while (parallel == false) {
     float dist = getDistance();
@@ -62,7 +62,7 @@ void turnToFace(float timerange, float error) {
       parallel = true;
     } else {
       moveWithTime(pi, 0.5, 0, timerange);
-      if ((newDist - dist) > 0) {   // rotate if not parallel
+      if ((newDist - dist) > 0) {  // rotate if not parallel
         moveWithTime(0, 0, -1, 100);
       } else {
         moveWithTime(0, 0, 1, 100);
@@ -70,30 +70,30 @@ void turnToFace(float timerange, float error) {
     }
   }
 
-  moveWithTime(pi, 0.5, 0, timerange/2); // recenter
+  moveWithTime(pi, 0.5, 0, timerange / 2);  // recenter
 }
 
 void moveToOtherFace() {
-  moveWithTime(0, 1, 0, 500/strafePS);
-  moveWithTime(0, 0, -1, (pi/2)/(rotatePS/1000));
-  moveWithTime(0, 1, 0, 500/strafePS);
+  moveWithTime(0, 1, 0, 500 / strafePS);
+  moveWithTime(0, 0, -1, (pi / 2) / (rotatePS / 1000));
+  moveWithTime(0, 1, 0, 500 / strafePS);
 }
 
 void alignY(float yVal, float error, float dir) {
   sStrafe();
 
-  float y = Enes100.getY();  
-  // float overcorrect = 2000;                        
+  float y = Enes100.getY();
+  // float overcorrect = 2000;
 
-  if ( y < (yVal-error) ) {     // if below the target y
-    while (y < (yVal-error)) {
-      y = Enes100.getY();                          
+  if (y < (yVal - error)) {  // if below the target y
+    while (y < (yVal - error)) {
+      y = Enes100.getY();
       move(pi, 0.5, 0);
     }
     // moveWithTime(0, 0.5, 0, overcorrect);
-  } else if ( y > (yVal+error) ) {  // if above target y
-    while (y > (yVal+error)) {
-      y = Enes100.getY();                          
+  } else if (y > (yVal + error)) {  // if above target y
+    while (y > (yVal + error)) {
+      y = Enes100.getY();
       move(0, 0.5, 0);
     }
     // moveWithTime(pi, 0.5, 0, overcorrect);
@@ -101,13 +101,13 @@ void alignY(float yVal, float error, float dir) {
 
   // if (y > (yVal-error)){
   //   while (y > (yVal-error)) {
-  //   y = Enes100.getY();                          
+  //   y = Enes100.getY();
   //   move(0, 0.5, 0);
 
-  // } 
+  // }
   // } else if (y < (yVal+error)) {
   //   while (y < (yVal+error)) {
-  //     y = Enes100.getY();                          
+  //     y = Enes100.getY();
   //     move(pi, 0.5, 0);
   //   }
   // }
@@ -116,15 +116,15 @@ void alignY(float yVal, float error, float dir) {
 }
 
 void alignX(float xVal, float error) {
-  float x = Enes100.getX();                          
+  float x = Enes100.getX();
 
-  while (x > (xVal+error)) {
-    x = Enes100.getX();                          
-    move(3*pi/2, 0.5, 0);
+  while (x > (xVal + error)) {
+    x = Enes100.getX();
+    move(3 * pi / 2, 0.5, 0);
   }
-  while (x < (xVal+error)) {
-    x = Enes100.getX();                          
-    move(pi/2, 0.5, 0);
+  while (x < (xVal + error)) {
+    x = Enes100.getX();
+    move(pi / 2, 0.5, 0);
   }
 }
 
@@ -132,7 +132,7 @@ void alignX(float xVal, float error) {
 int detectHeight(int angModifier = 0) {
   moveWithTime(pi, 0.5, 0, 500);
 
-  for (int ang = 180; ang > (60-angModifier); ang--) {
+  for (int ang = 180; ang > (60 - angModifier); ang--) {
     servo.write(ang);
     delay(30);
   }
@@ -142,7 +142,7 @@ int detectHeight(int angModifier = 0) {
   int angle = readPot();
 
   servo.attach(2);
-  for (int ang = (60-angModifier); ang < 180; ang++) {
+  for (int ang = (60 - angModifier); ang < 180; ang++) {
     servo.write(ang);
     delay(30);
   }
@@ -166,33 +166,82 @@ int detectLength(float error) {
   currentDist = getDistance();
   delay(1000);
 
-  float startTime = millis();                     // record starting time
+  float xI = Enes100.getX();
+  float yI = Enes100.getY();
+  //float startTime = millis();                     // record starting time
   while (abs(currentDist - startDist) < error) {  // strafe to the right until distance sensor reads a spike in readings -- the edge has been reached
     move(0, 0.5, 0);                              // strafe to the right
     currentDist = getDistance();
   }
-  float endTime = millis();  // record ending time
+  float xF = Enes100.getX();
+  float yF = Enes100.getY();
+  //float endTime = millis();  // record ending time
 
-  float deltaTime = endTime - startTime;
+  //float deltaTime = endTime - startTime;
 
   moveWithTime(pi, 0.5, 0, 200);  // move back into range of wall
 
-  return (deltaTime * (strafePS / 2) - 70);  // return estimate of mm of length
+  return int(1000 * norm(xI, yI, xF, yF));  // return estimate of mm of length
 }
 
 void findPath(float error) {
   // float startDist = getDistance();
   // float currentDist = getDistance();
-  if(Enes100.getY() < .75) { //If on lower path
-    while (getDistance() < 300 && Enes100.getY()<1.4) {
+  // if (Enes100.getY() < .75) {  //If on lower path
+  //   while (getDistance() < 300 && Enes100.getY() < 1.4) {
+  //     move(pi, 0.5, 0);
+  //   }
+  //   moveWithTime(pi, 0.5, 0, 2500);
+  // } else if (Enes100.getY() >= 0.75) {
+  //   while (getDistance() < 300 && Enes100.getY() > .4) {  //MAYBE SHOULD BE 5????
+  //     move(2 * pi, 0.5, 0);
+  //   }
+  //   moveWithTime(2 * pi, 0.5, 0, 2500);
+  // }
+
+  float startDist = getDistance();
+  float currentDist = getDistance();
+  if (Enes100.getY() > 1.25) {  //If on upper path
+    while (getDistance() < 300 && Enes100.getY() > 1.6) {  //MAYBE SHOULD BE 5????
+      move(2 * pi, 0.5, 0);
+    }
+    moveWithTime(2 * pi, 0.5, 0, 2500);
+  }
+    else if (Enes100.getY() <= 1.25) {
+    while (getDistance() < 300 && Enes100.getY() < .4) {
       move(pi, 0.5, 0);
     }
     moveWithTime(pi, 0.5, 0, 2500);
   }
-  else if(Enes100.getY() >=0.75){
-    while (getDistance() < 300 && Enes100.getY()>.4) {  //MAYBE SHOULD BE 5????
-      move(2*pi, 0.5, 0);
+}
+
+int anomalyLogic(int height, int length) {
+  if (height == 270) {
+    if (length == 180) {
+      length = 135;
+    } else if (length == 135) {
+      length = 180;
+    } else {
+      Enes100.print("Dimension logic error");
     }
-      moveWithTime(2*pi, 0.5, 0, 2500);
   }
+  if (height == 180) {
+    if (length == 270) {
+      length = 135;
+    } else if (length == 135) {
+      length = 270;
+    } else {
+      Enes100.print("Dimension logic error");
+    }
+  }
+  if (height == 135) {
+    if (length == 180) {
+      length = 270;
+    } else if (length == 270) {
+      length = 180;
+    } else {
+      Enes100.print("Dimension logic error");
+    }
+  }
+  return length;
 }
--- the-vedauntless/the-vedauntless.ino
+++ the-vedauntless/the-vedauntless.ino
@@ -1,6 +1,6 @@
 // THE VEDAUNTLESS -- FINAL CODE
 
-float batteryLevel = 1.40;
+float batteryLevel = 1.65;  //Full battery ~1.7
 
 // Import modules
 #include "Enes100.h"
@@ -41,58 +41,62 @@ void setup() {
     // NAVIGATE TO SITE
     Enes100.println("Navigating to Crash Site...");
     sForward();
-    navToSite(150); // navigate within 125 mm
+    navToSite(150);  // navigate within 150 mm
 
     // LENGTH MEASUREMENT
     sStrafe();
     Enes100.println("Detecting Length...");
-    turnToFace(250, 10);  // turn so movement will be parallel to the face
-    int length = detectLength(100); // error distance
-    Enes100.print("LENGTH: ");
-    Enes100.print(length);
-    Enes100.println("");
-    if (length <= 150) { length = 135; } 
-    else if (length > 150 && length <= 245) { length = 180; } 
-    else { length = 270; }
-    Enes100.mission(LENGTH, length);
-    moveWithTime(pi, 0.5, 0, length/strafePS); // move to middle of face
+    turnToFace(250, 10);             // turn so movement will be parallel to the face
+    int length = detectLength(100);  // error distance
+    if (length <= 150) {
+      length = 135;
+    } else if (length > 150 && length <= 245) {
+      length = 180;
+    } else {
+      length = 270;
+    }
+    moveWithTime(pi, 0.5, 0, length / strafePS);  // move to middle of face
 
     // HEIGHT MEASUREMENT
     Enes100.println("Detecting Height...");
     servo.attach(2);
-    int height = detectHeight(10); 
-    Enes100.println(height); 
-    if (height < 16) { height = 135; } 
-    else if (height < 19 && height > 16) { height = 180; } 
-    else { height = 270; }
-    Enes100.mission(HEIGHT, height);
+    int height = detectHeight(10);
+    if (height <= 9) {
+      height = 135;
+    } else if (height < 19 && height > 10) {
+      height = 180;
+    } else {
+      height = 270;
+    }
+    // Enes100.mission(HEIGHT, height);
     servo.detach();
 
-    // ANOMALY DETECTION
+    // ANOMALY DETECTION & DIMENSION LOGIC
     if (detectAnomaly()) {
-      Enes100.mission(DIRECTION, pi/2);
+      Enes100.println("MISSION MESSAGE: The direction of the abnormality is in the +Y direction");
+      Enes100.mission(DIRECTION, pi / 2);
       digitalWrite(52, LOW);
       sForward();
-      moveWithTime(3*pi/2, 0.5, 0, 5000);
-      turnToTheta(0, pi/20);
+      moveWithTime(3 * pi / 2, 0.5, 0, 5000);
+      turnToTheta(0, pi / 20);
       sForward();
-      moveWithTime(pi/2, 0.5, 0, 4000);
-      alignY(1, 0.1, 0);            // align to middle of field
+      moveWithTime(pi / 2, 0.5, 0, 4000);
 
     } else {
       Enes100.mission(DIRECTION, 0);
-      // moveWithTime(0, 0, 0.5, (pi/2)*1500/rotatePS);
-      turnToTheta(0, pi/20);
+      turnToTheta(0, pi / 20);
       digitalWrite(52, LOW);
       sForward();
-      moveWithTime(pi/2, 0.5, 0, 4000); 
-      //turnToTheta(pi/4, pi/20);
+      moveWithTime(pi / 2, 0.5, 0, 4000);
       turnToTheta(0, 0.1);
-      alignY(1, 0.1, 0);
-      move(0, 0, 0);
+      //logic
+      length = anomalyLogic(height, length);
     }
 
-    delay(2000);
+    Enes100.mission(LENGTH, length);
+    Enes100.mission(HEIGHT, height);
+
+    delay(1000);
 
     // OBSTACLE NAVIGATION
     Enes100.println("Navigating Obstacles...");
@@ -102,80 +106,35 @@ void setup() {
     float xmid = 1.96;
     float error = 0.1;
 
-    turnToTheta(0, pi/20);          // face towards obstacles
+    turnToTheta(0, pi / 20);  // face towards obstacles
     delay(500);
     sStrafe();
-    // alignY(1.25, error, 0);            // align to middle of field
-    // delay(500);
-    // turnToTheta(pi/4, pi/20);
-    // delay(500);
-    // turnToTheta(0, pi/20);
-    // delay(500);
 
-    // alignX(1, error);
-    // turnToTheta(0, pi/20);          // reset orientation again 
-
-    // delay(2000);
-
-    // if (getDistance() < 300) {      // if there is an obstacle there, know that there isn't one behind it
-    //   midBlocked = true;
-    //   Enes100.println("Middle path is blocked.");
-    // } else { Enes100.println("Middle path is open."); }
-
-    // delay(4000);
-
-    // // turnToTheta(pi/4, pi/20);       // turn to check for topfront obstacle
-    // alignY(1.5, error, 0);
-
-    // if (getDistance() < 300) {
-    //   topBlocked = true;
-    //   Enes100.println("Top path is blocked.");
-    // } else { Enes100.println("Top path is open."); }
-
-    // delay(1000);
-
-    // turnToTheta(0, pi/20);          // turn back to straight
     alignY(1, error, 0);
 
-    // if (midBlocked && topBlocked) {     // therefore, lower front is open and top back is open
-    //   alignY(0.5, error, 0);
-    //   moveUntilBlocked(80, 0.5);
-    //   alignY(1.5, error, 0);
-    //   moveUntilBlocked(150, 1);     // move through limbo
-    // } else if (midBlocked && topBlocked == false) {   // go through top, then through middle, and then through limbo
-    //   alignY(1.5, error, 0);
-    //   moveUntilBlocked(80, 0.5);
-    //   alignY(1, error, 0);
-    //   moveUntilBlocked(200, 1);
-    //   alignY(1.5, error, 0);
-    //   moveUntilBlocked(150, 1);
-    // } else if (midBlocked == false && topBlocked) {   // go through middle, then through top
-    //   moveUntilBlocked(80, 0.5);
-    //   alignY(1.5, error, 0);
-    //   turnToTheta(pi/4, pi/20);
-    //   turnToTheta(0, pi/20);
-    //   sForward();
-    //   moveWithTime(pi/2, 0.5, 0, 4000);
-    //   moveUntilBlocked(120, 0.5);
-    // }
-    if(Enes100.getY() > 1) {
+    if (Enes100.getY() > 1) {
       alignY(.5, .05, 0);
-    }
-    else {
+    } else {
       alignY(1.5, .05, 0);
     }
-    while(Enes100.getX()<3.75) { //While before limbo
+    while (Enes100.getX() < 3.25) {  //While before limbo
       sForward();
-      moveUntilBlocked(125,0.5); //Move until hit block
-      Enes100.println("Distance: ");
-      Enes100.print(getDistance());
-      sStrafe();
-      findPath(error);
-      sForward();
-      Enes100.println("Realigning...");
-      turnToTheta(0, 0.1);
-    }     
+      moveUntilBlocked(125, 0.5);  //Move until hit block
+      if (Enes100.getX() < 3.25) {  //While before limbo
+        sStrafe();
+        findPath(error);
+        sForward();
+        Enes100.println("Realigning...");
+        turnToTheta(0, 0.1);
+      }
+    }
   }
+  servo.attach(2);
+  for (int ang = 180; ang > (60); ang--) {
+    servo.write(ang);
+    delay(30);
+  }
+  servo.detach();
 }
 
 void loop() {
