e2cafb8adbd383c39cac8a25fc550edd4800cd7d
Haroon <haroon.a.amanullah@gmail.com>
2023-12-03 12:59:06 -0500

    turnToMission

---
+++ the-vedauntless/temp.h
@@ -0,0 +1,28 @@
+bool turnToMission(float totalTime, float error) {
+  // assume already turned in the general direction of the mission.
+
+  float currentDist = getDistance();
+  float startTime = millis();
+  float dist = 0;
+
+  // scan left
+  while ((millis() - startTime) < totalTime/2)  {     // detect within time limit
+    move(0, 0, -0.5);
+    dist = getDistance();
+    if (abs(currentDist - dist) > error) {    // if catches edge
+      moveWithTime(0, 0, 0.5, 200);           // turn back to face
+      return true;                            // and exit function
+    } 
+  }                                           // if fails to catch edge, then it was already turned past to begin with
+  moveWithTime(0, 0, 0.5, totalTime/2);       // reset to where it started
+
+  // scan right
+  while ((millis() - startTime) < totalTime/2)  {     // detect within time limit
+    move(0, 0, 0.5);
+    dist = getDistance();
+    if (abs(currentDist - dist) > error) {    // if catches edge
+      moveWithTime(0, 0, -0.5, 200);          // turn back to face
+      return true;                            // and exit function
+    } 
+  }                                           // if fails to catch edge, then it was already turned past to begin with
+}
