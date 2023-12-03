dc9e12f2b7c03730dbc0bc7538990eb7dc3dafe4
Haroon <haroon.a.amanullah@gmail.com>
2023-12-03 13:26:26 -0500

    Update temp.h

--- the-vedauntless/temp.h
+++ the-vedauntless/temp.h
@@ -6,25 +6,35 @@ bool turnToMission(float totalTime, float error) {
   float dist = 0;
 
   // scan left
-  while ((millis() - startTime) < totalTime/2)  {     // detect within time limit
-    move(0, 0, -0.5);
-    dist = getDistance();
-    if (abs(currentDist - dist) > error) {    // if catches edge
-      Enes100.println("Found while detecting left");
-      moveWithTime(0, 0, 0.5, 500);           // turn back to face
-      return true;                            // and exit function
-    } 
-  }                                           // if fails to catch edge, then it was already turned past to begin with
-  moveWithTime(0, 0, 0.5, totalTime/2);       // reset to where it started
+  // while (abs(currentDist - dist) < error && (millis() - startTime) < totalTime/2)  {     // detect within time limit
+  //   move(pi, 0.5, 0);
+  //   dist = getDistance();
+  //   if (abs(currentDist - dist) > error) {    // if catches edge
+  //     Enes100.println("Found while detecting left");
+  //     moveWithTime(0, 0, 0.5, 500);           // turn back to face
+  //     return true;                            // and exit function
+  //   } 
+  // }                                           // if fails to catch edge, then it was already turned past to begin with
+  // moveWithTime(0, 0.5, 0, totalTime/2);       // reset to where it started
 
-  // scan right
-  while ((millis() - startTime) < totalTime/2)  {     // detect within time limit
-    move(0, 0, 0.5);
+  // startTime = millis();
+
+  // // scan right
+  // while ((millis() - startTime) < totalTime/2)  {     // detect within time limit
+  //   move(0, 0.5, 0);
+  //   dist = getDistance();
+  //   if (abs(currentDist - dist) > error) {    // if catches edge
+  //     Enes100.println("Found while detecting right");
+  //     moveWithTime(pi, 0.5, 0, 500);          // turn back to face
+  //     return true;                            // and exit function
+  //   } 
+  // }                                           // if fails to catch edge, then it was already turned past to begin with
+
+  moveWithTime(pi, 0.5, 0, totalTime/2);          // move to the left
+  while (abs(currentDist - dist) < error)  {      // until catches edge, move to the right
+    move(0, 0.5, 0);
     dist = getDistance();
-    if (abs(currentDist - dist) > error) {    // if catches edge
-      Enes100.println("Found while detecting right");
-      moveWithTime(0, 0, -0.5, 500);          // turn back to face
-      return true;                            // and exit function
-    } 
-  }                                           // if fails to catch edge, then it was already turned past to begin with
+  }        
+  Enes100.println("Found while detecting left");
+  moveWithTime(0, 0.5, 0, 500);           // move to face
 }
